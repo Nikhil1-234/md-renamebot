5a08cc2ae40e4a5a6db25c5b9698e14b4c87e9c4
Nikhil1-234 <142384405+Nikhil1-234@users.noreply.github.com>
2023-08-20 22:12:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 class Config(object):
     BANNED_USERS = []
     DOWNLOAD_LOCATION = "./DOWNLOADS" 
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH" "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+    API_ID = int(os.environ.get("API_ID","22299340"))
+    API_HASH = os.environ.get("API_HASH" "mongodb+srv://jidovi4576:B6HywpSzpSqZZ3W@clusterbot2.ezfn734.mongodb.net/?retryWrites=true&w=majority")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6439927958:AAHhSthTQi1DjQpqwZOC2vBjy_n_thi5sC8")
+    OWNER_ID = int(os.environ.get("OWNER_ID","5380609667'))
     AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", None)
-    DATABASE_URI = os.environ.get("DATABASE_URI", None)
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://jidovi4576:B6HywpSzpSqZZ3W@clusterbot2.ezfn734.mongodb.net/?retryWrites=true&w=majority")
